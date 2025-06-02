b9b2d5993f774db10970cfe9327e7de93ef94c03
Mihir <mihir.singh2904@gmail.com>
2025-06-02 22:51:18 +0530

    ffmpeg added

---
+++ src/utils/ffmpeg.js
@@ -0,0 +1,15 @@
+import ffmpeg from "fluent-ffmpeg";
+
+const getVideoDuration = (videoPath) => {
+    return new Promise((resolve, reject) => {
+        ffmpeg.ffprobe(videoPath, (err, metadata) => {
+            if (err) {
+                reject("Error extracting video duration");
+            } else {
+                resolve(metadata.format.duration);
+            }
+        });
+    });
+};
+
+export default getVideoDuration;
